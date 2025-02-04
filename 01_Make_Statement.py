6e896ce72d9b1b8feb832b12dfd326b5d8f640a6
SacredSeth <141595269+SacredSeth@users.noreply.github.com>
2025-02-04 14:45:45 +1300

    Make Statement Completed

---
+++ 01_Make_Statement.py
@@ -0,0 +1,8 @@
+# Functions
+def make_statement(statement, decoration):
+    """Decorates headings with chosen emoji/ASCII character"""
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main
+make_statement("Hello World", "🌍")
