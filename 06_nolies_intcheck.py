6bf95389db67c4d280863ecee93856470199e85a
SacredSeth <141595269+SacredSeth@users.noreply.github.com>
2025-02-14 14:54:38 +1300

    No lies number checker completed

---
+++ 06_nolies_intcheck.py
@@ -0,0 +1,97 @@
+# Functions
+def num_check(question, datatype=int, low=0, high=None, exit_code="xxx"):
+    """ Function to make sure user inputs an integer / float that is within parameters """
+
+    # check if parameters are set
+    if low != None and high == None:
+        ror = f" that is at least {low}"
+        check = 0
+    elif low != None and high != None:
+        ror = f" that is between {low} and {high}"
+        check = 1
+    elif low == None and high != None:
+        ror = f" that at most {high}"
+        check = 2
+    else:
+        ror = ""
+        check = 3
+
+    # get correct error message for data type
+    if datatype == int:
+        err = "Please enter an integer"
+    else:
+        err = "Please enter a number"
+
+    error = err + ror
+
+    while True:
+
+        # tests for exit code
+        test_exit = input(question).lower()
+
+        if test_exit == exit_code or test_exit == exit_code[0]:
+            return "exit"
+
+        # try statement for checking that it is of the correct datatype
+        try:
+            response = datatype(test_exit)
+
+            # Different calculations for set values of low and high
+            if check == 0:
+                if response >= low:
+                    return response
+                else:
+                    print(error)
+
+            elif check == 1:
+                if low <= response <= high:
+                    return response
+                else:
+                    print(error)
+
+            elif check == 2:
+                if response <= high:
+                    return response
+                else:
+                    print(error)
+
+            else:
+                return response
+
+        except ValueError:
+            print(err)
+
+
+def not_blank(question):
+    """checks that a user has not left a response blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("You cannot leave this blank, Please enter a response\n")
+
+
+# Main
+
+# loop for testing
+while 1:
+    print()
+
+    # ask the user their name
+    name = not_blank("Name: ")
+
+    # ask user for their age
+    age = num_check("Age: ")
+
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} is too young")
+        continue
+    elif age > 120:
+        print(f"{name} is too old")
+        continue
+    else:
+        print(f"{name} bought a ticket")
